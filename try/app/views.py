8152ea24fd8558dbb056ab23eb5670b287a50320
essonam <essonam18@gmail.com>
2023-08-15 11:22:21 +0100

    initial commit

---
+++ try/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    context = {"message":"bonjour a tous"}
+    template = loader.get_template("app/index.html")
+    return HttpResponse(template.render(context,request))
